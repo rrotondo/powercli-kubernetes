c13021a513db20f21f35f62e9731fd45aa70e65c
Riccardo Rotondo <net.ricky@gmail.com>
2020-02-28 12:15:37 +0100

    Add yaml script, ps script and secret

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # powercli-kubernetes
 
+A project to execute PowerShell Core script inside a Kubernetes cluster
+
+## Requirements 
+
+* Kubernetes >= 1.6.0
+* PowerShell >= 6.2.3
+
+## Configuration
+
+### ConfigMap
+CronJob uses script defined in ConfigMap
+
+#### Snapshot
+Define a ConfigMap containing the script [VMSnapshot.ps1](https://baltig.infn.it/infn-ct/powercli-kubernetes/blob/master/script/VMSnapshot.ps1) as follow:
+```console
+kubectl create configmap powercli-script-snap --from-file=script/VMSnapshot.ps1
+```
+
+### Secret
+CronJob uses two different secrets:
+1. [powercli-secret.yaml](https://baltig.infn.it/infn-ct/powercli-kubernetes/blob/master/secret/powercli-secret.yaml) contains a variable for the server hostname
+2. A file contains credential used by the script for authentication on the server. Create the file with the following PowerShell commands:
+```console
+$User = "Username"   
+$Pass = "Password" | ConvertTo-SecureString -AsPlainText -Force 
+$Credentials = New-Object System.Management.Automation.PSCredential($User,$Pass)
+$Credentials | Export-CliXml -Path MyCrential.xml
+```
+After create a secret containing the credential file
+```console
+kubectl create secret generic powercli-secret-credential --from-file=secret/MyCredential.xml
+```
---
+++ script/VMSnapshot.ps1
@@ -0,0 +1,76 @@
+# Copyright 2019 Istituto Nazionale di Fisica Nucleare
+# salvatore.monforte@ct.infn.it
+# riccardo.rotondo@ct.infn.it
+
+param(
+    [Parameter(ValueFromPipeline, Mandatory)]
+    [string] $server,
+    [string] $credfile,
+    [string] $schedule = "daily"
+)
+
+Set-PowerCLIConfiguration -ParticipateInCEIP:$false -InvalidCertificateAction Ignore -Confirm:$false
+
+
+If ( ! (Get-module VMware.VimAutomation.core )) {
+
+    get-module -name PowerCli* -ListAvailable |  Import-Module
+}
+
+#Stop an error from occurring when a transcript is already stopped
+$ErrorActionPreference = "SilentlyContinue"
+Stop-Transcript | out-null
+
+#Reset the error level before starting the transcript
+$ErrorActionPreference = "Continue"
+
+#Connect to the server using the credentials file      
+$creds = Import-Clixml -Path $credfile
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($creds.Password)
+$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+Connect-VIServer -Server $server -User $creds.UserName -Password $password
+
+    
+    #Get all VMs having ScheduleSnapshot set to $schedule 
+    $VMs = @(Get-VM | Get-Annotation -CustomAttribute "SnapshotSchedule" |
+        Where-Object { $_.Value -eq $schedule } | Sort-Object AnnotatedEntity)
+
+    ForEach ($VM in $VMs) {
+
+        $name = $VM.AnnotatedEntity.Name
+        $snapName = Get-Date -Format "yyyyMMdd-HHmm"
+
+        # Creating snapshot
+        Write-Host "Creating snapshot for VM $name as $snapName"
+        try{
+            New-Snapshot -VM $VM.AnnotatedEntity -Name $snapName -Quiesce:$true -Confirm:$false
+        }
+        catch [VMware.VimAutomation.Sdk.Types.V1.ErrorHandling.VimException.VimException]{
+            Write-Host "VimException Caught"
+            Write-Host $_
+            Write-Host $_.ScriptStackTrace
+        }
+
+        # Cleaning up by removing oldest snaps
+        $SNAPs = Get-Snapshot -VM $VM.AnnotatedEntity 
+        $Retain = (Get-Annotation -Entity $VM.AnnotatedEntity -CustomAttribute "SnapshotRetain").Value -as [int]
+
+        if ($SNAPs.Length -gt $Retain) {
+            Write-Host "Removing oldest snapshots for VM $name"
+            try{
+                Remove-Snapshot -Snapshot (
+                    $SNAPs | Sort-Object -Descending Created | 
+                    Select-Object -Last ($SNAPs.Length - $Retain)
+                ) -Confirm:$false
+            }
+            catch {
+                Write-Host "General Exception Caught"
+                Write-Host $_
+                Write-Host $_.ScriptStackTrace
+            }
+        }
+    }
+
+#Disconnect
+Disconnect-VIServer -Force -Confirm:$false
